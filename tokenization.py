ffb9482f09f40cede27b9793f828de09e0e7cd18
XuShenghua <884826402@qq.com>
2019-12-20 00:16:31 +0800

    修改tokenization文件

--- tokenization.py
+++ tokenization.py
@@ -162,11 +162,12 @@ class FullTokenizer(object):
   """Runs end-to-end tokenziation."""
 
   def __init__(self, vocab_file, do_lower_case=True):
+    bpe_vocab_file = ''
     self.vocab = load_vocab(vocab_file)
     self.inv_vocab = {v: k for k, v in self.vocab.items()}
     self.basic_tokenizer = BasicTokenizer(do_lower_case=do_lower_case)
-    self.wordpiece_tokenizer = WordpieceTokenizer(vocab=self.vocab)
-
+    # self.wordpiece_tokenizer = WordpieceTokenizer(vocab=self.vocab)
+    self.wordpiece_tokenizer = WordpieceTokenizerXSH(vocab=self.vocab,bpe_vocab_file=bpe_vocab_file)
   def tokenize(self, text):
     split_tokens = []
     for token in self.basic_tokenizer.tokenize(text):
@@ -325,6 +326,95 @@ class WordpieceTokenizer(object):
 
     text = convert_to_unicode(text)
 
+    output_tokens = []
+    for token in whitespace_tokenize(text):
+      chars = list(token)
+      if len(chars) > self.max_input_chars_per_word:
+        output_tokens.append(self.unk_token)
+        continue
+
+      is_bad = False
+      start = 0
+      sub_tokens = []
+      while start < len(chars):
+        end = len(chars)
+        cur_substr = None
+        while start < end:
+          substr = "".join(chars[start:end])
+          if start > 0:
+            substr = "##" + substr
+          if substr in self.vocab:
+            cur_substr = substr
+            break
+          end -= 1
+        if cur_substr is None:
+          is_bad = True
+          break
+        sub_tokens.append(cur_substr)
+        start = end
+
+      if is_bad:
+        output_tokens.append(self.unk_token)
+      else:
+        output_tokens.extend(sub_tokens)
+    return output_tokens
+class WordpieceTokenizerXSH(object):
+  """Runs WordPiece tokenziation."""
+
+  def __init__(self, vocab,bpe_vocab_file, unk_token="[UNK]", max_input_chars_per_word=200):
+    self.vocab = vocab
+    self.unk_token = unk_token
+    self.max_input_chars_per_word = max_input_chars_per_word
+    bpe_vocab = collections.OrderedDict()
+    index = 0
+    with tf.gfile.GFile(bpe_vocab_file, "r") as reader:
+      while True:
+        token = convert_to_unicode(reader.readline())
+        if not token:
+          break
+        token = token.strip()
+        bpe_vocab[index] = token
+        index += 1
+    self.bpe_vocab = bpe_vocab
+
+  def tokenize(self, text):
+    """Tokenizes a piece of text into its word pieces.
+
+    This uses a greedy longest-match-first algorithm to perform tokenization
+    using the given vocabulary.
+
+    For example:
+      input = "unaffable"
+      output = ["un", "##aff", "##able"]
+
+    Args:
+      text: A single token or whitespace separated tokens. This should have
+        already been passed through `BasicTokenizer.
+
+    Returns:
+      A list of wordpiece tokens.
+    """
+
+    text = convert_to_unicode(text)
+    text = text.strip()
+    is_return = True
+    output_tokens = []
+    try:
+      bpe_tokens = [self.bpe_vocab[self.vocab[text]].split(' ')]
+      for token in bpe_tokens:
+        if token.endswith('@@'):
+          token =token[:-2]
+        if token in self.vocab:
+          output_tokens.append(token)
+        elif  '##'+token in self.vocab:
+          output_tokens.append('##'+token)
+        else:
+          is_return = False
+          break
+    except:
+      is_return = False
+    if is_return:
+      return [text] + output_tokens
     output_tokens = []
     for token in whitespace_tokenize(text):
       chars = list(token)
